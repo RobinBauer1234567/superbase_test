3fb94ad22c79668ccd668554caf30452dc18087d
RobinBauer1234567 <bob.bauer.05@gmail.com>
2026-09-16 09:48:47 +0200

    Route matches to restored legacy pitch screen

--- lib/screens/premier_league/matches_screen.dart
+++ lib/screens/premier_league/matches_screen.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:premier_league/screens/match_details_screen_fixed.dart';
+import 'package:premier_league/screens/match_details_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:premier_league/screens/team_screen.dart';
